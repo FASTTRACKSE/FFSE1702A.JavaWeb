e7055b03db8f9099f8140dc7fc8499fb59e88a22
ffse1702005 <ffse1702005@st.fasttrack.edu.vn>
2018-06-26 09:35:49 +0700

    jsf

--- FFSE1702005/JSFCRUD/src/com/jsf/crud/db/operations/DatabaseOperation.java
+++ FFSE1702005/JSFCRUD/src/com/jsf/crud/db/operations/DatabaseOperation.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Map;
@@ -18,42 +19,81 @@ public class DatabaseOperation {
 	public static Connection connObj;
 	public static ResultSet resultSetObj;
 	public static PreparedStatement pstmt;
+	int currentpage;
+	
+	public int getCurrentpage() {
+		return currentpage;
+	}
+
+	public void setCurrentpage(int currentpage) {
+		this.currentpage = currentpage;
+	}
 
 	/* Method To Establish Database Connection */
-	public static Connection getConnection(){  
-		try{  
-			Class.forName("com.mysql.jdbc.Driver");     
-			String db_url ="jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=utf-8",
-					db_userName = "ffse1702005",
-					db_password = "ffse1702@12345";
-			connObj = DriverManager.getConnection(db_url,db_userName,db_password);  
-		} catch(Exception sqlException) {  
+	public static Connection getConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String db_url = "jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=utf-8",
+					db_userName = "ffse1702005", db_password = "ffse1702@12345";
+			connObj = DriverManager.getConnection(db_url, db_userName, db_password);
+		} catch (Exception sqlException) {
 			sqlException.printStackTrace();
-		}  
+		}
 		return connObj;
+	}
+	
+	public int getLastpage(int recordPerPage) throws SQLException {
+		int numberOfRecords = numberOfRecord();
+
+		int lastPage = 0;
+		if((numberOfRecords%recordPerPage)==0) {
+			lastPage = (numberOfRecords / recordPerPage);
+		}else {
+			lastPage = (numberOfRecords / recordPerPage) + 1;
+		}
+		return lastPage;
+	}
+
+	public int numberOfRecord() throws SQLException{
+		try {
+			stmtObj = getConnection().createStatement();
+			PreparedStatement ps = (PreparedStatement) connObj.prepareStatement("select count(*) from student_record");
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+			return rs.getInt("count(*)");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			connObj.close();
+		}
+		return 0;
 	}
 
 	/* Method To Fetch The Student Records From Database */
-	public static ArrayList<StudentBean> getStudentsListFromDB() {
-		ArrayList<StudentBean> studentsList = new ArrayList<StudentBean>();  
+	public static ArrayList<StudentBean> getStudentsListFromDB(int page, int psize) {
+		ArrayList<StudentBean> studentsList = new ArrayList<StudentBean>();
+		int start = (page - 1) * psize;
+		if(start<0) {
+			start=0;
+		}	
 		try {
-			stmtObj = getConnection().createStatement();    
-			resultSetObj = stmtObj.executeQuery("select * from student_record");    
-			while(resultSetObj.next()) {  
-				StudentBean stuObj = new StudentBean(); 
-				stuObj.setId(resultSetObj.getInt("student_id"));  
-				stuObj.setName(resultSetObj.getString("student_name"));  
-				stuObj.setEmail(resultSetObj.getString("student_email"));  
-				stuObj.setPassword(resultSetObj.getString("student_password"));  
-				stuObj.setGender(resultSetObj.getString("student_gender"));  
-				stuObj.setAddress(resultSetObj.getString("student_address"));  
-				studentsList.add(stuObj);  
-			}   
+			stmtObj = getConnection().createStatement();
+			resultSetObj = stmtObj.executeQuery("select * from student_record limit " + start +"," + psize);
+			while (resultSetObj.next()) {
+				StudentBean stuObj = new StudentBean();
+				stuObj.setId(resultSetObj.getInt("student_id"));
+				stuObj.setName(resultSetObj.getString("student_name"));
+				stuObj.setEmail(resultSetObj.getString("student_email"));
+				stuObj.setPassword(resultSetObj.getString("student_password"));
+				stuObj.setGender(resultSetObj.getString("student_gender"));
+				stuObj.setAddress(resultSetObj.getString("student_address"));
+				studentsList.add(stuObj);
+			}
 			System.out.println("Total Records Fetched: " + studentsList.size());
 			connObj.close();
-		} catch(Exception sqlException) {
+		} catch (Exception sqlException) {
 			sqlException.printStackTrace();
-		} 
+		}
 		return studentsList;
 	}
 
@@ -61,8 +101,9 @@ public class DatabaseOperation {
 	public static String saveStudentDetailsInDB(StudentBean newStudentObj) {
 		int saveResult = 0;
 		String navigationResult = "";
-		try {      
-			pstmt = getConnection().prepareStatement("insert into student_record (student_name, student_email, student_password, student_gender, student_address) values (?, ?, ?, ?, ?)");			
+		try {
+			pstmt = getConnection().prepareStatement(
+					"insert into student_record (student_name, student_email, student_password, student_gender, student_address) values (?, ?, ?, ?, ?)");
 			pstmt.setString(1, newStudentObj.getName());
 			pstmt.setString(2, newStudentObj.getEmail());
 			pstmt.setString(3, newStudentObj.getPassword());
@@ -70,10 +111,10 @@ public class DatabaseOperation {
 			pstmt.setString(5, newStudentObj.getAddress());
 			saveResult = pstmt.executeUpdate();
 			connObj.close();
-		} catch(Exception sqlException) {
+		} catch (Exception sqlException) {
 			sqlException.printStackTrace();
 		}
-		if(saveResult !=0) {
+		if (saveResult != 0) {
 			navigationResult = "studentsList.xhtml?faces-redirect=true";
 		} else {
 			navigationResult = "createStudent.xhtml?faces-redirect=true";
@@ -87,24 +128,24 @@ public class DatabaseOperation {
 		System.out.println("editStudentRecordInDB() : Student Id: " + studentId);
 
 		/* Setting The Particular Student Details In Session */
-		Map<String,Object> sessionMapObj = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+		Map<String, Object> sessionMapObj = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
 
 		try {
-			stmtObj = getConnection().createStatement();    
-			resultSetObj = stmtObj.executeQuery("select * from student_record where student_id = "+studentId);    
-			if(resultSetObj != null) {
+			stmtObj = getConnection().createStatement();
+			resultSetObj = stmtObj.executeQuery("select * from student_record where student_id = " + studentId);
+			if (resultSetObj != null) {
 				resultSetObj.next();
-				editRecord = new StudentBean(); 
+				editRecord = new StudentBean();
 				editRecord.setId(resultSetObj.getInt("student_id"));
 				editRecord.setName(resultSetObj.getString("student_name"));
 				editRecord.setEmail(resultSetObj.getString("student_email"));
 				editRecord.setGender(resultSetObj.getString("student_gender"));
 				editRecord.setAddress(resultSetObj.getString("student_address"));
-				editRecord.setPassword(resultSetObj.getString("student_password")); 
+				editRecord.setPassword(resultSetObj.getString("student_password"));
 			}
 			sessionMapObj.put("editRecordObj", editRecord);
 			connObj.close();
-		} catch(Exception sqlException) {
+		} catch (Exception sqlException) {
 			sqlException.printStackTrace();
 		}
 		return "/editStudent.xhtml?faces-redirect=true";
@@ -113,29 +154,30 @@ public class DatabaseOperation {
 	/* Method Used To Update Student Record In Database */
 	public static String updateStudentDetailsInDB(StudentBean updateStudentObj) {
 		try {
-			pstmt = getConnection().prepareStatement("update student_record set student_name=?, student_email=?, student_password=?, student_gender=?, student_address=? where student_id=?");    
-			pstmt.setString(1,updateStudentObj.getName());  
-			pstmt.setString(2,updateStudentObj.getEmail());  
-			pstmt.setString(3,updateStudentObj.getPassword());  
-			pstmt.setString(4,updateStudentObj.getGender());  
-			pstmt.setString(5,updateStudentObj.getAddress());  
-			pstmt.setInt(6,updateStudentObj.getId());  
+			pstmt = getConnection().prepareStatement(
+					"update student_record set student_name=?, student_email=?, student_password=?, student_gender=?, student_address=? where student_id=?");
+			pstmt.setString(1, updateStudentObj.getName());
+			pstmt.setString(2, updateStudentObj.getEmail());
+			pstmt.setString(3, updateStudentObj.getPassword());
+			pstmt.setString(4, updateStudentObj.getGender());
+			pstmt.setString(5, updateStudentObj.getAddress());
+			pstmt.setInt(6, updateStudentObj.getId());
 			pstmt.executeUpdate();
-			connObj.close();			
-		} catch(Exception sqlException) {
+			connObj.close();
+		} catch (Exception sqlException) {
 			sqlException.printStackTrace();
 		}
 		return "/studentsList.xhtml?faces-redirect=true";
 	}
 
 	/* Method Used To Delete Student Record From Database */
-	public static String deleteStudentRecordInDB(int studentId){
+	public static String deleteStudentRecordInDB(int studentId) {
 		System.out.println("deleteStudentRecordInDB() : Student Id: " + studentId);
 		try {
-			pstmt = getConnection().prepareStatement("delete from student_record where student_id = "+studentId);  
-			pstmt.executeUpdate();  
+			pstmt = getConnection().prepareStatement("delete from student_record where student_id = " + studentId);
+			pstmt.executeUpdate();
 			connObj.close();
-		} catch(Exception sqlException){
+		} catch (Exception sqlException) {
 			sqlException.printStackTrace();
 		}
 		return "/studentsList.xhtml?faces-redirect=true";
