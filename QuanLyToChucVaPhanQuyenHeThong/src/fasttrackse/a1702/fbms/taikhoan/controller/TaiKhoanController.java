d7fc8b2df29ecddd7b23ef190cc864b452107742
HiepTV <35910113+ffse1701003@users.noreply.github.com>
2018-08-22 14:31:30 +0700

    Module ChucNangPhongBan
    CRUD, Paging

--- QuanLyToChucVaPhanQuyenHeThong/src/fasttrackse/a1702/fbms/taikhoan/controller/TaiKhoanController.java
+++ QuanLyToChucVaPhanQuyenHeThong/src/fasttrackse/a1702/fbms/taikhoan/controller/TaiKhoanController.java
@@ -1,4 +1,4 @@
-package fasttrackse.a1702.fbms.chucnang.controller;
+package fasttrackse.a1702.fbms.taikhoan.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,19 +10,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import fasttrackse.a1702.fbms.chucnang.model.entities.ChucNang;
-import fasttrackse.a1702.fbms.chucnang.service.ChucNangService;
+import fasttrackse.a1702.fbms.taikhoan.model.entities.TaiKhoan;
+import fasttrackse.a1702.fbms.taikhoan.service.TaiKhoanService;
 
 @Controller
-@RequestMapping("/chuc-nang")
-public class ChucNangController {
-
+@RequestMapping("/tai-khoan")
+public class TaiKhoanController {
 	@Autowired
-	ChucNangService cnService;
+	TaiKhoanService taiKhoanService;
 
-	@RequestMapping(value = "/danh-sach", method = RequestMethod.GET)
-	public String danhSachChucNang(Model model, @RequestParam(name = "page", required = false, defaultValue = "1") int currentPage) {
-		int totalRecords = cnService.findAll().size();
+	public TaiKhoanService getTaiKhoanService() {
+		return taiKhoanService;
+	}
+
+	public void setTaiKhoanService(TaiKhoanService taiKhoanService) {
+		this.taiKhoanService = taiKhoanService;
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String listTaiKhoan(Model model,
+			@RequestParam(name = "page", required = false, defaultValue = "1") int currentPage) {
+		int totalRecords = taiKhoanService.findAll().size();
 		int recordsPerPage = 10;
 		int totalPages = 0;
 		if ((totalRecords / recordsPerPage) % 2 == 0) {
@@ -32,67 +40,67 @@ public class ChucNangController {
 		}
 		int startPosition = recordsPerPage * (currentPage - 1);
 
-		model.addAttribute("listChucNang", cnService.findAllForPaging(startPosition, recordsPerPage));
+		model.addAttribute("listTaiKhoan", taiKhoanService.findAllForPaging(startPosition, recordsPerPage));
 		model.addAttribute("lastPage", totalPages);
 		model.addAttribute("currentPage", currentPage);
-		return "chucnang/list";
+		return "taikhoan/list";
 	}
 
 	@RequestMapping(value = "/them-moi", method = RequestMethod.GET)
 	public String addForm(Model model, final RedirectAttributes redirectAttributes) {
-		model.addAttribute("chucNang", new ChucNang());
-		return "chucnang/add_form";
+		model.addAttribute("taiKhoan", new TaiKhoan());
+		return "taikhoan/add_form";
 	}
 
 	@RequestMapping(value = "/them-moi/luu", method = RequestMethod.POST)
-	public String doAdd(Model model, @ModelAttribute("chucNang") ChucNang cn,
+	public String doAdd(Model model, @ModelAttribute("taiKhoan") TaiKhoan cn,
 			final RedirectAttributes redirectAttributes) {
 		try {
-			cnService.save(cn);
+			taiKhoanService.addNew(cn);
 			redirectAttributes.addFlashAttribute("messageSuccess", "Thêm mới thành công..");
 		} catch (Exception e) {
 			redirectAttributes.addFlashAttribute("messageError", "Lỗi. Xin thử lại!");
 		}
-		return "redirect:/chuc-nang/danh-sach";
+		return "redirect:/tai-khoan/danh-sach";
 	}
 
 	@RequestMapping(value = "/sua/{code}", method = RequestMethod.GET)
 	public String editForm(@PathVariable("code") String code, Model model) {
-		model.addAttribute("chucNang", cnService.getChucNangByCode(code));
-		return "chucnang/edit_form";
+//		model.addAttribute("taiKhoan", taiKhoanService.getChucNangByCode(code));
+		return "taikhoan/edit_form";
 	}
 
 	@RequestMapping(value = "/sua/luu", method = RequestMethod.POST)
-	public String doEdit(Model model, @ModelAttribute("chucNang") ChucNang cn,
+	public String doEdit(Model model, @ModelAttribute("taiKhoan") TaiKhoan cn,
 			final RedirectAttributes redirectAttributes) {
 		try {
-			cnService.update(cn);
+			taiKhoanService.update(cn);
 			redirectAttributes.addFlashAttribute("messageSuccess", "Thành công..");
 		} catch (Exception e) {
 			redirectAttributes.addFlashAttribute("messageError", "Lỗi. Xin thử lại!");
 		}
-		return "redirect:/chuc-nang/danh-sach";
+		return "redirect:/tai-khoan/danh-sach";
 	}
 
 	@RequestMapping(value = "/kich-hoat/{code}", method = RequestMethod.GET)
 	public String active(@PathVariable("code") String ma_chuc_nang, final RedirectAttributes redirectAttributes) {
 		try {
-			cnService.active(ma_chuc_nang);
+			taiKhoanService.active(ma_chuc_nang);
 			redirectAttributes.addFlashAttribute("messageSuccess", "Thành công..");
 		} catch (Exception e) {
 			redirectAttributes.addFlashAttribute("messageError", "Lỗi. Xin thử lại!");
 		}
-		return "redirect:/chuc-nang/danh-sach";
+		return "redirect:/tai-khoan/danh-sach";
 	}
-	
+
 	@RequestMapping(value = "/xoa/{code}", method = RequestMethod.GET)
 	public String delete(@PathVariable("code") String ma_chuc_nang, final RedirectAttributes redirectAttributes) {
 		try {
-			cnService.delete(ma_chuc_nang);
+			taiKhoanService.delete(ma_chuc_nang);
 			redirectAttributes.addFlashAttribute("messageSuccess", "Xóa thành công..");
 		} catch (Exception e) {
 			redirectAttributes.addFlashAttribute("messageError", "Lỗi. Xin thử lại!");
 		}
-		return "redirect:/chuc-nang/danh-sach";
+		return "redirect:/tai-khoan/danh-sach";
 	}
 }
---
+++ QuanLyToChucVaPhanQuyenHeThong/src/fasttrackse/a1702/fbms/taikhoan/model/entities/TaiKhoan.java
@@ -0,0 +1,61 @@
+package fasttrackse.a1702.fbms.taikhoan.model.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tai_khoan")
+public class TaiKhoan {
+	@Id
+	@Column(name = "ma_nhan_vien")
+	private String maNhanVien;
+	
+	@Column(name = "mat_khau")
+	private String matKhau;
+	
+	@Column(name = "trang_thai")
+	private int trangThai;
+
+	public String getMaNhanVien() {
+		return maNhanVien;
+	}
+
+	public void setMaNhanVien(String maNhanVien) {
+		this.maNhanVien = maNhanVien;
+	}
+
+	public String getMatKhau() {
+		return matKhau;
+	}
+
+	public void setMatKhau(String matKhau) {
+		this.matKhau = matKhau;
+	}
+
+	public int getTrangThai() {
+		return trangThai;
+	}
+
+	public void setTrangThai(int trangThai) {
+		this.trangThai = trangThai;
+	}
+
+	public TaiKhoan() {
+		super();
+	}
+
+	public TaiKhoan(String maNhanVien, String matKhau, int trangThai) {
+		super();
+		this.maNhanVien = maNhanVien;
+		this.matKhau = matKhau;
+		this.trangThai = trangThai;
+	}
+
+	@Override
+	public String toString() {
+		return "TaiKhoan [maNhanVien=" + maNhanVien + ", matKhau=" + matKhau + ", trangThai=" + trangThai + "]";
+	}
+
+}
