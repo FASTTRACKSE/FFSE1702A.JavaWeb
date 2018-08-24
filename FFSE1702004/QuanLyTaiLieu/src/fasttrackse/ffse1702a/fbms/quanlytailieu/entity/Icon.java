5d3bcfc4883e5ff57e5048c34da3b42a3d0c9cae
DUNGDA1998 <ffse1702004@st.fasttrack.edu.vn>
2018-08-24 22:45:17 +0700

    please

---
+++ FFSE1702004/QuanLyTaiLieu/src/fasttrackse/ffse1702a/fbms/quanlytailieu/entity/Icon.java
@@ -0,0 +1,50 @@
+package fasttrackse.ffse1702a.fbms.quanlytailieu.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "icon")
+public class Icon implements Serializable {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Column(name = "ma_icon")
+	private String ma_icon;
+
+	@Column(name = "hinh_anh")
+	private String hinh_anh;
+
+	public String getMa_icon() {
+		return ma_icon;
+	}
+
+	public void setMa_icon(String ma_icon) {
+		this.ma_icon = ma_icon;
+	}
+
+	public String getHinh_anh() {
+		return hinh_anh;
+	}
+
+	public void setHinh_anh(String hinh_anh) {
+		this.hinh_anh = hinh_anh;
+	}
+
+}
